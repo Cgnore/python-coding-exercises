73c72ef24d95295fdaa30cb271773fb052cf9524
Cgnore <fahrettinalibalci@stu.aydin.edu.tr>
2026-06-11 14:53:13 +0300

    first day project has been added.

---
+++ day_1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Program!")
+hometown_name = input("What is your hometown?\n")
+pet_name = input("What is your pet name?\n")
+print("Your band name could be: " + pet_name + " " + hometown_name)
